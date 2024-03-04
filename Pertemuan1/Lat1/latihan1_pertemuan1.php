0b914a98ee5b09b4a744ab5cafcb40c5b91293f4
MuhammadFadhillahHafidh789 <161955660+MuhammadFadhillahHafidh789@users.noreply.github.com>
2024-03-05 01:50:41 +0700

    Add files via upload

---
+++ Pertemuan1/Lat1/latihan1_pertemuan1.php
@@ -0,0 +1,8 @@
+<?php
+print"<pre>";
+print"<center><font size=14 color='red'><b>BIODATA</b></font></center>";
+print"<font size=14 color='blue'>Nama    : <b>Muhammad Fadhillah Hafidh</b></font></br>";
+print"<font size=14 color='blue'>Email   : fadhillahhafidh4@gmail.com</font></br>";
+print"<font size=14 color='blue'>Jurusan : Tenik Informatika</font></br>";
+print"<font size=14 color='blue'>Hoby    : Membuat Musik</font>";
+?>
---
+++ Pertemuan1/lat2/latihan2_pertemuan1.php
@@ -0,0 +1,5 @@
+<?php
+print"<pre>";
+print"<center><font size=14 color='red'><b>Hello Saya M. Fadhillah Hafidh</b></font></center>";
+print"<font size=14 color='blue'><b><marquee>Belajar PHP Awalnya sulit bagi saya</marquee></b></font></br>";
+?>
